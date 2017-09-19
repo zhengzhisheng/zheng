c9aeefb9b11ad628c937e9fa26019aa359aa7147
郑志胜 <zhengzhisheng@zhengzhishengdeMacBook-Pro.local>
2017-09-19 22:50:22 +0800

    Initial commit

---
+++ src/main/java/com/zheng/blog/ZhengApplication.java
@@ -0,0 +1,12 @@
+package com.zheng.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhengApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZhengApplication.class, args);
+	}
+}
